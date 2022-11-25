909c69d52c027e7933e5a1c023dc0944211a5322
Fantom Asasin <qwertyytrewq10923845@gmail.com>
2022-11-25 15:46:45 +0300

    init

---
+++ src/main/java/com/astradev_back/astradev_back/AstradevBackApplication.java
@@ -0,0 +1,13 @@
+package com.astradev_back.astradev_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstradevBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstradevBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astradev_back/astradev_back/AstradevBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astradev_back.astradev_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstradevBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
